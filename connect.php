ce1439f9042298a4f9a045a25b7f2de9ec57011c
TheeeX <karanjeet96@gmail.com>
2015-08-22 17:36:33 +0530

    database config

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect ("localhost","root","ididit","artistarc");
+$con=mysqli_connect ("localhost","username","password","db_name");
 if (mysqli_connect_errno()) {
 echo "failed to connect mysql: ". mysqli_connect_error();
 }else echo "sucess!";
